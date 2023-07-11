8e1fa84429ad2ea7f819ebb1e10feb58b35bf425
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-07-11 13:30:55 +0200

    feat: Make project use dotenv

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from dotenv import load_dotenv
+
+load_dotenv()
 
 apikey = os.environ.get('OPENAI_API_KEY')
 
